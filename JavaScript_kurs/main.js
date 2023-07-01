690a40aae78353a8cc39bdd2933769cde8c2d168
Satch2619 <ninho13@hotmail.de>
2023-07-01 21:12:03 +0200

    refresh010723

---
+++ JavaScript_kurs/main.js
@@ -0,0 +1,15 @@
+// IF Statements
+
+let age = prompt("Trage dein Alter ein");
+
+if(age >= 18) {
+  // Wird ausgeführt wenn Bedinung wahr wird
+  console.log("Eintritt gewährt")
+  // alert("Eintritt gewährt")
+} else {
+  // Wird in jedem andren Fall ausgeführt
+  console.log("Kein Eintritt")
+  // alert("Kein Eintritt")
+}
+
+
